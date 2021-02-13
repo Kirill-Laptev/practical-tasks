ca1a6d678a378cfa563b48caa1536f9ad2d861a6
Kirill <laptevkirill.wwc@gmail.com>
2021-02-13 18:07:56 +0300

    Updates

---
+++ precache-manifest.0e2334d6f427bae22d72a4623fe22570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826b42e9288f3188b90d6b8d60820492",
+    "url": "/practical-tasks/index.html"
+  },
+  {
+    "revision": "7eea8ee3fa84ff18f1ee",
+    "url": "/practical-tasks/static/css/main.abe7bb96.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/practical-tasks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practical-tasks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eea8ee3fa84ff18f1ee",
+    "url": "/practical-tasks/static/js/main.70e6dd66.chunk.js"
+  },
+  {
+    "revision": "23049a0b34347a37b736",
+    "url": "/practical-tasks/static/js/runtime-main.6fb566d4.js"
+  }
+]);
